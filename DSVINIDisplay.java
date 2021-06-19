88c3e69f153c7682e694b813aa6c85d57d0b709c
Keian Rao <keian.rao@gmail.com>
2021-06-19 17:48:13 -0400

    Tentative structure.

--- DSVINIDisplay.java
+++ DSVINIDisplay.java
@@ -1,1 +1,38 @@
 
+import java.util.Map;
+import java.util.HashMap;
+
+public class DSVINIDisplay {
+
+//  Helper classes  \\  //  \\  //  \\  //  \\
+
+class INISection {
+	String name;
+	Map<String, String> mappings;
+}
+
+class DSVtoINIMapping {
+	String[] keys;
+	String sectionNameKey;
+}
+
+
+
+//  Interface   //  \\  //  \\  //  \\  //  \\
+
+public static INISection translate(String dsv, DSVtoINIMapping keyMapping)
+{
+
+}
+
+
+
+//  Main    \\  //  \\  //  \\  //  \\  //  \\
+
+public static void main(String... args) {
+		
+}
+
+}
+
+//  \\  //  \\  //  \\  //  \\  //  \\  //  \\
